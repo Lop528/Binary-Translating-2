d49537695a3c6654734ef6c92267dddd0786e20e
Liam Seymour <spaceturatle@gmail.com>
2024-09-17 09:12:31 -0500

    Initial Commit

---
+++ Binary Translating 2/Binary_Translating_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Binary_Translating_2App.swift
+//  Binary Translating 2
+//
+//  Created by Liam K. Seymour on 9/17/24.
+//
+
+import SwiftUI
+
+@main
+struct Binary_Translating_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Binary Translating 2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Binary Translating 2
+//
+//  Created by Liam K. Seymour on 9/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
