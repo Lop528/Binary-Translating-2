5fde6700df74d6389e724737b08acec50e68b354
Liam Seymour <spaceturatle@gmail.com>
2024-09-19 08:31:07 -0500

    View

---
+++ Binary Translating 2/HexaView.swift
@@ -0,0 +1,18 @@
+//
+//  HexaView.swift
+//  Binary Translating 2
+//
+//  Created by Liam K. Seymour on 9/19/24.
+//
+
+import SwiftUI
+
+struct HexaView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HexaView()
+}
