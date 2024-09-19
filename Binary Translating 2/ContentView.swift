dac88fca464f87754f0839daf16e323cbaa4dacd
Liam Seymour <spaceturatle@gmail.com>
2024-09-19 08:50:10 -0500

    ijijpk

--- Binary Translating 2/ContentView.swift
+++ Binary Translating 2/ContentView.swift
@@ -11,10 +11,30 @@ struct ContentView: View {
     
     
     
+    @State var enteredNumber = ""
+    @State var finalString = ""
     var body: some View {
+        let number = Int(enteredNumber) ?? 0
         VStack {
-            Text("Binary")
+            Text("Binary Translator")
+            Divider()
+            HStack {
+                TextField("Enter Number", text: $enteredNumber)
+                Button("Translate") {
+                    finalString = "\(binaryString(from: number))"
+                }
+                .foregroundColor(.cyan)
+            }
+            Text("\(finalString)")
+
         }
+    }
+    func binaryString(from number: Int) -> String {
+        guard number >= 0 else {
+            return "Invalid number"
+        }
+        
+        return String(number, radix: 2)
     }
 }
 
--- Binary Translating 2/HexaView.swift
+++ Binary Translating 2/HexaView.swift
@@ -1,19 +1,41 @@
 //
-//  HexaView.swift
+//  ContentView.swift
 //  Binary Translating 2
 //
-//  Created by Liam K. Seymour on 9/19/24.
+//  Created by Liam K. Seymour on 9/17/24.
 //
 
 import SwiftUI
 
 struct HexaView: View {
+    @State var enteredNumber = ""
+    @State var finalString = ""
     var body: some View {
+        let number = Int(enteredNumber) ?? 0
         VStack {
-            Text("Hexadecimal")
+            Text("Hexadecimal Translator")
+            Divider()
+            HStack {
+                TextField("Enter Number", text: $enteredNumber)
+                Button("Translate") {
+                    finalString = "\(binaryString(from: number))"
+                }
+                .foregroundColor(.cyan)
+            }
+            Text("\(finalString)")
+
         }
+        .padding()
+    }
+    func binaryString(from number: Int) -> String {
+        guard number >= 0 else {
+            return "Invalid number"
+        }
+        
+        return String(number, radix: 16)
     }
 }
+
 
 #Preview {
     HexaView()
