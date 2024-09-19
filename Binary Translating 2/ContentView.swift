d0a3d1d64ecc27e32043b2cac457bbeedaa00cd2
Liam Seymour <spaceturatle@gmail.com>
2024-09-19 08:32:37 -0500

    daw

--- Binary Translating 2/ContentView.swift
+++ Binary Translating 2/ContentView.swift
@@ -11,7 +11,6 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Text("Binary")
-            Text("Hexadecimal")
         }
         .padding()
     }
--- Binary Translating 2/HexaView.swift
+++ Binary Translating 2/HexaView.swift
@@ -9,7 +9,9 @@ import SwiftUI
 
 struct HexaView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Hexadecimal")
+        }
     }
 }
 
