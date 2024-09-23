ce659e86e8f9f8579fdfa5e92099c1125938f403
Liam Seymour <spaceturatle@gmail.com>
2024-09-23 08:38:08 -0500

    adw

--- Binary Translating 2/ContentView.swift
+++ Binary Translating 2/ContentView.swift
@@ -13,24 +13,45 @@ struct ContentView: View {
     var body: some View {
         let number = Int(enteredNumber) ?? 0
         VStack {
+            Spacer()
             Text("Translator")
-            TextField("Enter Number", text: $enteredNumber)
+                .font(.custom("American Typewriter", size: 35))
+            ZStack {
+                RoundedRectangle(cornerRadius: 10.0)
+                    .frame(width: 380, height: 40)
+                    .foregroundColor(.gray)
+                    .opacity(0.2)
+                TextField("Enter Number", text: $enteredNumber)
+                    .font(.custom("American Typewriter", size: 18))
+                    .padding(7)
+                
+            }
             Divider()
             HStack {
-                HStack {
-                    Button("Binary") {
-                        finalString = "\(binaryString(from: number))"
-                    }
+                Button("Binary") {
+                    finalString = "\(binaryString(from: number))"
                 }
+                .font(.custom("American Typewriter", size: 18))
                 .foregroundColor(.cyan)
+                
+                .foregroundColor(.cyan)
+                Text("|")
+                    .bold()
                 Button("Hexadecimal") {
                     finalString = "\(hexaString(from: number))"
                 }
+                .font(.custom("American Typewriter", size: 18))
                 .foregroundColor(.cyan)
+                
             }
             Text("\(finalString)")
+                .font(.custom("American Typewriter", size: 25))
+            Spacer()
 
         }
+        
+        .offset(y:-20)
+        .shadow(radius: 10)
         .padding()
     }
     func binaryString(from number: Int) -> String {
